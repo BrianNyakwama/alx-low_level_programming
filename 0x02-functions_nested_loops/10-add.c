79cb3543c3b88ede67851f50c033e18677ef6adf
Brian Nyakwama <damugrobrian1@gmail.com>
2023-02-22 17:46:02 -0800

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * add - adds two integers and returns the result
+  * @p: first number to be computed
+  * @q: second number to be computed
+  *
+  * Return: Integer value of the sum
+  */
+int add(int p, int q)
+{
+	int r;
+
+	r = p + q;
+
+	return (r);
+}
