1f88a8cdb330e0148c409ae4f66c255b117af8d9
Brian Nyakwama <damugrobrian1@gmail.com>
2023-03-02 14:54:16 -0800

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * string_toupper - changes all lowercase letters of a string to uppercase
+  * @c: string in lowercase
+  * Return: string in uppercase
+  */
+char *string_toupper(char *c)
+{
+	int i = 0;
+
+	while (c[i])
+	{
+		if (c[i] >= 97 && c[i] <= 122)
+			c[i] = c[i] - 32;
+		i++;
+	}
+	return (c);
+}
