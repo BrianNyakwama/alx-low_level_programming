1464e82c4dfdeabe3ec553b8e9f270c87cd70e54
Brian Nyakwama <damugrobrian1@gmail.com>
2023-03-13 14:57:46 -0700

    it isnt the mountains ahead to climb that wear you out, its the peeble in your shoe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -14,14 +14,13 @@ char *argstostr(int ac, char **av)
 	char *aout;
 	int c, i, j, ia;
 
-	if (ac == 0)
+	if (ac == 0 || av == NULL)
 		return (NULL);
 	for (c = i = 0; i < ac; i++)
 	{
-		if (av[i] == NULL)
-			return (NULL);
 		for (j = 0; av[i][j] != '\0'; j++)
 			c++;
+		c++;
 	}
 	aout = malloc((c + 1) * sizeof(char));
 
